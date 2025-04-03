e4ce9f0a80a20f3f14bdc737dc875eb9e9a486a6
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-04-03 14:24:25 -0500

    feat(layout): add Header and Footer components to default layout

---
+++ components/general/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-footer color="grey lighten-1" class="text-center">
+    <div>&copy; {{ new Date().getFullYear() }} - Powered by Me</div>
+  </v-footer>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app theme="light">
+    <Header />
+    <v-main>
+      <v-container>
+        <slot />
+      </v-container>
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import Header from '~/components/general/Header.vue'
+import Footer from '~/components/general/Footer.vue'
+</script>
+
+<style>
+.container {
+  padding: 16px;
+  max-width: 1200px;
+  margin: auto;
+}
+</style>

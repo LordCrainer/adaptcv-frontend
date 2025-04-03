3b411681cbf052e6998b0a9e382d3cf51295ab20
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-04-03 14:25:45 -0500

    feat(card): add CardDefault component with customizable title, icon, and color

---
+++ components/card/CardDefault.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card
+    rounded="lg"
+    class="mx-auto my-4"
+    max-width="600"
+    :color="color"
+    :prepend-icon="icon">
+    <template v-slot:title>
+      <span class="font-weight-black">{{ $t(`${title}`) }}</span>
+    </template>
+    <v-card-text class="bg-white pt-4">
+      <slot></slot>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    default: 'mdi-account'
+  },
+  color: {
+    type: String,
+    default: 'primary'
+  }
+})
+</script>

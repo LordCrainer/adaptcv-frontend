1a61f7d865402f9ec70410d352d2eda5b9601e98
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-04-04 13:30:20 -0500

    feat(card): update CardDefault component layout and styling

--- components/card/CardDefault.vue
+++ components/card/CardDefault.vue
@@ -1,14 +1,9 @@
 <template>
-  <v-card
-    rounded="lg"
-    class="mx-auto my-4"
-    max-width="600"
-    :color="color"
-    :prepend-icon="icon">
+  <v-card rounded="lg" class="mx-auto" max-width="800" :color="color">
     <template v-slot:title>
-      <span class="font-weight-black">{{ title }}</span>
+      <span class="font-weight-black text-primary">{{ title }}</span>
     </template>
-    <v-card-text class="bg-white pt-4">
+    <v-card-text class="pt-4">
       <slot></slot>
     </v-card-text>
   </v-card>
@@ -17,18 +12,15 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 
-const props = defineProps({
+defineProps({
   title: {
     type: String,
     required: true
   },
-  icon: {
-    type: String,
-    default: 'mdi-account'
-  },
+  icon: String,
   color: {
     type: String,
-    default: 'primary'
+    default: 'white'
   }
 })
 </script>

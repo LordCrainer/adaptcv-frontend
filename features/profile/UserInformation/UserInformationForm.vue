34ce986f61e9c09932fd7216c8d3496afff155ad
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-04-11 01:51:56 -0500

    feat(profile): add UserInformationForm component for user profile input

---
+++ features/profile/UserInformation/UserInformationForm.vue
@@ -0,0 +1,128 @@
+<template>
+  <v-col cols="12" md="8" class="d-flex flex-column ga-2">
+    <div class="d-flex justify-end py-4">
+      <v-btn color="secondary" variant="outlined" @click="fillInformation">
+        {{ $t('profile.personalInfo.fillInformation') }}
+      </v-btn>
+    </div>
+    <div class="d-flex flex-column ga-2">
+      <v-text-field
+        prepend-inner-icon="mdi-account"
+        v-model="formData.name"
+        :label="$t('profile.personalInfo.fullname')"
+        placeholder="Ej: John Doe"
+        variant="outlined"
+        :rules="[rules.required($t('profile.personalInfo.fullname'))]"
+        required></v-text-field>
+      <v-text-field
+        v-model="formData.email"
+        prepend-inner-icon="mdi-email"
+        :label="$t('profile.personalInfo.email')"
+        placeholder="Ej: example@example.com"
+        variant="outlined"
+        type="email"
+        required></v-text-field>
+
+      <v-text-field
+        v-model="formData.phone"
+        prepend-inner-icon="mdi-phone"
+        :label="$t('profile.personalInfo.phone')"
+        placeholder="Ej: +1234567890"
+        variant="outlined"
+        type="tel"></v-text-field>
+
+      <v-row class="py-2">
+        <v-col cols="6" class="py-0">
+          <v-text-field
+            v-model="formData.country"
+            :label="$t('profile.personalInfo.country')"
+            prepend-inner-icon="mdi-map"
+            placeholder="Ej: country"
+            variant="outlined"></v-text-field>
+        </v-col>
+        <v-col cols="6" class="py-0">
+          <v-text-field
+            v-model="formData.city"
+            :label="$t('profile.personalInfo.city')"
+            prepend-inner-icon="mdi-city"
+            placeholder="Ej: city"
+            variant="outlined"></v-text-field>
+        </v-col>
+      </v-row>
+    </div>
+    <div>
+      <v-select
+        clearable
+        :label="$t('profile.personalInfo.areaProfession')"
+        variant="outlined"
+        prepend-inner-icon="mdi-briefcase"
+        :items="Object.keys(areasProfession)"
+        v-model="formData.areaProfession"
+        autocomplete></v-select>
+      <v-select
+        v-if="formData.areaProfession"
+        prepend-inner-icon="mdi-account-hard-hat"
+        :label="$t('profile.personalInfo.profession')"
+        variant="outlined"
+        :items="professions"
+        v-model="formData.profession"
+        autocomplete></v-select>
+    </div>
+  </v-col>
+  <v-col>
+    <v-textarea
+      v-model="formData.summary"
+      rounded="md"
+      flat
+      prepend-inner-icon="mdi-file-document"
+      :label="$t('profile.personalInfo.summary')"
+      variant="outlined"
+      placeholder="Ej: A brief summary about yourself"
+      name="summary"></v-textarea>
+  </v-col>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { FormData } from '..'
+import { areasProfession } from '../constants'
+
+const { rules } = useRules()
+
+const formData = ref<FormData>({
+  name: '',
+  profession: '',
+  areaProfession: undefined,
+  email: '',
+  phone: '',
+  address: '',
+  country: '',
+  city: '',
+  summary: ''
+})
+
+const emit = defineEmits(['update:formData'])
+
+const professions = ref<string[]>([])
+
+watch(
+  () => formData.value.areaProfession,
+  (newArea) => {
+    if (newArea) {
+      formData.value.profession = ''
+      professions.value = areasProfession[newArea]
+    }
+  }
+)
+
+watch(
+  formData,
+  (newFormData) => {
+    emit('update:formData', newFormData)
+  },
+  { deep: true }
+)
+
+const fillInformation = () => {
+  console.log('Filling information...')
+}
+</script>
---
+++ features/profile/UserInformation/index.vue
@@ -0,0 +1,45 @@
+<style>
+.profile-photo {
+  width: 175px;
+  height: 175px;
+}
+</style>
+<template>
+  <v-form>
+    <v-row>
+      <v-col cols="12" md="4" class="d-flex flex-1-0 justify-center">
+        <v-card class="profile-photo rounded-circle">
+          <v-img
+            class="profile-photo"
+            aspect-ratio="1/1"
+            cover
+            src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-img>
+          <input
+            type="file"
+            accept="image/*"
+            @change="onFileChange"
+            class="d-none"
+            ref="fileInput" />
+        </v-card>
+      </v-col>
+      <UserInformationForm />
+    </v-row>
+  </v-form>
+</template>
+
+<script setup lang="ts">
+import UserInformationForm from './UserInformationForm.vue'
+
+function onFileChange(event: Event) {
+  const input = event.target as HTMLInputElement
+  if (input.files && input.files.length > 0) {
+    const file = input.files[0]
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      // Handle the file data here
+      console.log(e.target?.result)
+    }
+    reader.readAsDataURL(file)
+  }
+}
+</script>

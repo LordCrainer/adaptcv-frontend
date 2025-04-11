3902f4cf0f3187da9c7eeef1bb91a916ee6f35a7
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-04-10 23:34:35 -0500

    refactor(Locales): restructure profile translations for improved organization and maintainability

--- locales/en-US.ts
+++ locales/en-US.ts
@@ -1,5 +1,66 @@
 import { en as $vuetify } from 'vuetify/locale'
 
+const profile: TranslationKeys['profile'] = {
+  personalInfo: {
+    title: 'Personal Information',
+    fullname: 'Full Name',
+    areaProfession: 'Area of Profession',
+    profession: 'Profession',
+    email: 'Email',
+    phone: 'Phone',
+    address: 'Address',
+    summary: 'Summary',
+    city: 'City',
+    country: 'Country',
+    fillInformation: 'Fill Information',
+    aboutMe: 'About Me'
+  },
+  experience: {
+    title: 'Experience',
+    jobTitle: 'Job Title',
+    company: 'Company',
+    position: 'Position',
+    startDate: 'Start Date',
+    endDate: 'End Date',
+    description: 'Description'
+  },
+  education: {
+    title: 'Education',
+    fieldOfStudy: 'Field of Study',
+    degree: 'Degree',
+    institution: 'Institution',
+    startDate: 'Start Date',
+    endDate: 'End Date',
+    secondary: 'Secondary',
+    highSchool: 'High School',
+    technical: 'Technical',
+    undergraduate: 'Undergraduate',
+    graduate: 'Graduate',
+    masters: 'Masters',
+    doctorate: 'Doctorate'
+  },
+  skills: {
+    title: 'Skills',
+    skill: 'Skill',
+    less1year: 'Less than 1 year',
+    '1to2years': '1 to 2 years',
+    '2to3years': '2 to 3 years',
+    '5to10years': '5 to 10 years',
+    '10plusyears': '10+ years',
+    job: 'Job',
+    yearsOfExperience: 'Years of Experience'
+  },
+  languages: {
+    title: 'Languages',
+    language: 'Language',
+    level: 'Level',
+    beginner: 'Beginner',
+    intermediate: 'Intermediate',
+    advanced: 'Advanced',
+    native: 'Native'
+  }
+}
+
 export default defineI18nLocale(async () => {
   return {
     actions: {
@@ -40,66 +101,7 @@ export default defineI18nLocale(async () => {
       confirmPassword: 'Confirm Password',
       signUp: 'Sign Up'
     },
-    profile: {
-      personalInfo: {
-        title: 'Personal Information',
-        fullname: 'Full Name',
-        areaProfession: 'Area of Profession',
-        profession: 'Profession',
-        email: 'Email',
-        phone: 'Phone',
-        address: 'Address',
-        summary: 'Summary',
-        city: 'City',
-        country: 'Country',
-        fillInformation: 'Fill Information',
-        aboutMe: 'About Me'
-      },
-      experience: {
-        title: 'Experience',
-        jobTitle: 'Job Title',
-        company: 'Company',
-        position: 'Position',
-        startDate: 'Start Date',
-        endDate: 'End Date',
-        description: 'Description'
-      },
-      education: {
-        title: 'Education',
-        fieldOfStudy: 'Field of Study',
-        degree: 'Degree',
-        institution: 'Institution',
-        startDate: 'Start Date',
-        endDate: 'End Date',
-        secondary: 'Secondary',
-        highSchool: 'High School',
-        technical: 'Technical',
-        undergraduate: 'Undergraduate',
-        graduate: 'Graduate',
-        masters: 'Masters',
-        doctorate: 'Doctorate'
-      },
-      skills: {
-        title: 'Skills',
-        skill: 'Skill',
-        less1year: 'Less than 1 year',
-        '1to2years': '1 to 2 years',
-        '2to3years': '2 to 3 years',
-        '5to10years': '5 to 10 years',
-        '10plusyears': '10+ years',
-        job: 'Job',
-        yearsOfExperience: 'Years of Experience'
-      },
-      languages: {
-        title: 'Languages',
-        language: 'Language',
-        level: 'Level',
-        beginner: 'Beginner',
-        intermediate: 'Intermediate',
-        advanced: 'Advanced',
-        native: 'Native'
-      }
-    },
+    profile: profile,
     $vuetify: $vuetify
   } as TranslationKeys
 })

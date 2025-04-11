f421e64278b8d7f09c716fcd3d8c5529efe79e37
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-04-10 23:33:47 -0500

    fix(Locales): update locale files to use defineI18nLocale for better structure and maintainability

--- locales/en-US.ts
+++ locales/en-US.ts
@@ -1,114 +1,105 @@
 import { en as $vuetify } from 'vuetify/locale'
 
-export default {
-  actions: {
-    add: 'Add',
-    edit: 'Edit',
-    delete: 'Delete',
-    save: 'Save',
-    cancel: 'Cancel',
-    submit: 'Submit',
-    options: 'Options'
-  },
-  common: {
-    home: 'Home',
-    about: 'About',
-    services: 'Services',
-    contact: 'Contact',
-    testimonials: 'Testimonials',
-    blog: 'Blog',
-    portfolio: 'Portfolio',
-    privacyPolicy: 'Privacy Policy',
-    termsOfService: 'Terms of Service',
-    email: 'Email',
-    noData: 'No data available'
-  },
-  login: {
-    email: 'email',
-    password: 'password',
-    login: 'Login',
-    forgotPassword: 'Forgot Password?',
-    rememberMe: 'Remember Me',
-    signUp: 'Sign Up',
-    signIn: 'Sign In'
-  },
-  registration: {
-    username: 'Username',
-    email: 'Email',
-    password: 'Password',
-    confirmPassword: 'Confirm Password',
-    signUp: 'Sign Up'
-  },
-  profile: {
-    personalInfo: {
-      title: 'Personal Information',
-      fullname: 'Full Name',
-      areaProfession: 'Area of Profession',
-      profession: 'Profession',
+export default defineI18nLocale(async () => {
+  return {
+    actions: {
+      add: 'Add',
+      edit: 'Edit',
+      delete: 'Delete',
+      save: 'Save',
+      cancel: 'Cancel',
+      submit: 'Submit',
+      options: 'Options'
+    },
+    common: {
+      home: 'Home',
+      about: 'About',
+      services: 'Services',
+      contact: 'Contact',
+      testimonials: 'Testimonials',
+      blog: 'Blog',
+      portfolio: 'Portfolio',
+      privacyPolicy: 'Privacy Policy',
+      termsOfService: 'Terms of Service',
       email: 'Email',
-      phone: 'Phone',
-      address: 'Address',
-      summary: 'Summary',
-      city: 'City',
-      country: 'Country',
-      fillInformation: 'Fill Information',
-      aboutMe: 'About Me'
+      noData: 'No data available'
     },
-    experience: {
-      title: 'Experience',
-      jobTitle: 'Job Title',
-      company: 'Company',
-      position: 'Position',
-      startDate: 'Start Date',
-      endDate: 'End Date',
-      description: 'Description'
+    login: {
+      email: 'email',
+      password: 'password',
+      login: 'Login',
+      forgotPassword: 'Forgot Password?',
+      rememberMe: 'Remember Me',
+      signUp: 'Sign Up',
+      signIn: 'Sign In'
     },
-    education: {
-      title: 'Education',
-      fieldOfStudy: 'Field of Study',
-      degree: 'Degree',
-      institution: 'Institution',
-      startDate: 'Start Date',
-      endDate: 'End Date',
-      secondary: 'Secondary',
-      highSchool: 'High School',
-      technical: 'Technical',
-      undergraduate: 'Undergraduate',
-      graduate: 'Graduate',
-      masters: 'Masters',
-      doctorate: 'Doctorate'
+    registration: {
+      username: 'Username',
+      email: 'Email',
+      password: 'Password',
+      confirmPassword: 'Confirm Password',
+      signUp: 'Sign Up'
     },
-    skills: {
-      title: 'Skills',
-      skill: 'Skill',
-      less1year: 'Less than 1 year',
-      '1to2years': '1 to 2 years',
-      '2to3years': '2 to 3 years',
-      '5to10years': '5 to 10 years',
-      '10plusyears': '10+ years',
-      job: 'Job',
-      yearsOfExperience: 'Years of Experience'
+    profile: {
+      personalInfo: {
+        title: 'Personal Information',
+        fullname: 'Full Name',
+        areaProfession: 'Area of Profession',
+        profession: 'Profession',
+        email: 'Email',
+        phone: 'Phone',
+        address: 'Address',
+        summary: 'Summary',
+        city: 'City',
+        country: 'Country',
+        fillInformation: 'Fill Information',
+        aboutMe: 'About Me'
+      },
+      experience: {
+        title: 'Experience',
+        jobTitle: 'Job Title',
+        company: 'Company',
+        position: 'Position',
+        startDate: 'Start Date',
+        endDate: 'End Date',
+        description: 'Description'
+      },
+      education: {
+        title: 'Education',
+        fieldOfStudy: 'Field of Study',
+        degree: 'Degree',
+        institution: 'Institution',
+        startDate: 'Start Date',
+        endDate: 'End Date',
+        secondary: 'Secondary',
+        highSchool: 'High School',
+        technical: 'Technical',
+        undergraduate: 'Undergraduate',
+        graduate: 'Graduate',
+        masters: 'Masters',
+        doctorate: 'Doctorate'
+      },
+      skills: {
+        title: 'Skills',
+        skill: 'Skill',
+        less1year: 'Less than 1 year',
+        '1to2years': '1 to 2 years',
+        '2to3years': '2 to 3 years',
+        '5to10years': '5 to 10 years',
+        '10plusyears': '10+ years',
+        job: 'Job',
+        yearsOfExperience: 'Years of Experience'
+      },
+      languages: {
+        title: 'Languages',
+        language: 'Language',
+        level: 'Level',
+        beginner: 'Beginner',
+        intermediate: 'Intermediate',
+        advanced: 'Advanced',
+        native: 'Native'
+      }
     },
-    languages: {
-      title: 'Languages',
-      language: 'Language',
-      level: 'Level',
-      beginner: 'Beginner',
-      intermediate: 'Intermediate',
-      advanced: 'Advanced',
-      native: 'Native'
-    }
-  },
-  $vuetify: {
-    noDataText: 'No data available',
-    open: 'Open',
-    close: 'Close',
-    input: {
-      clear: 'Clear',
-      prependAction: 'Prepend Action',
-      appendAction: 'Append Action',
-      placeholder: 'Placeholder'
-    }
-  },
-  ...$vuetify
-} as TranslationKeys
+    $vuetify: $vuetify
+  } as TranslationKeys
+})
--- locales/es-ES.ts
+++ locales/es-ES.ts
@@ -1,101 +1,105 @@
 import { es as $vuetify } from 'vuetify/locale'
 
-export default {
-  actions: {
-    add: 'Agregar',
-    edit: 'Editar',
-    delete: 'Eliminar',
-    save: 'Guardar',
-    cancel: 'Cancelar',
-    submit: 'Enviar',
-    options: 'Opciones'
-  },
-  common: {
-    home: 'Inicio',
-    about: 'Acerca de',
-    services: 'Servicios',
-    contact: 'Contacto',
-    testimonials: 'Testimonios',
-    blog: 'Blog',
-    portfolio: 'Portafolio',
-    privacyPolicy: 'Política de privacidad',
-    termsOfService: 'Términos de servicio',
+const profile: TranslationKeys['profile'] = {
+  personalInfo: {
+    title: 'Información personal',
+    fullname: 'Nombre completo',
+    areaProfession: 'Área de profesión',
+    profession: 'Profesión',
     email: 'Correo electrónico',
-    noData: 'No hay datos disponibles'
+    phone: 'Teléfono',
+    address: 'Dirección',
+    summary: 'Resumen',
+    city: 'Ciudad',
+    country: 'País',
+    fillInformation: 'Rellena la información',
+    aboutMe: 'Acerca de mí'
   },
-  login: {
-    password: 'contraseña',
-    login: 'Iniciar sesión',
-    forgotPassword: '¿Olvidaste tu contraseña?',
-    rememberMe: 'Recuérdame',
-    signUp: 'Regístrate',
-    signIn: 'Iniciar sesión'
+  experience: {
+    title: 'Experiencia',
+    jobTitle: 'Título del trabajo',
+    company: 'Compañía',
+    position: 'Posición',
+    startDate: 'Fecha de inicio',
+    endDate: 'Fecha de finalización',
+    description: 'Descripción'
   },
-  registration: {
-    username: 'Nombre de usuario',
-    password: 'Contraseña',
-    confirmPassword: 'Confirmar contraseña',
-    signUp: 'Regístrate'
+  education: {
+    title: 'Educación',
+    fieldOfStudy: 'Campo de estudio',
+    degree: 'Título',
+    institution: 'Institución',
+    startDate: 'Fecha de inicio',
+    endDate: 'Fecha de finalización',
+    secondary: 'Secundaria',
+    highSchool: 'Escuela secundaria',
+    technical: 'Técnica',
+    undergraduate: 'Licenciatura',
+    graduate: 'Posgrado',
+    masters: 'Maestría',
+    doctorate: 'Doctorado'
   },
-  profile: {
-    personalInfo: {
-      title: 'Información personal',
-      fullname: 'Nombre completo',
-      areaProfession: 'Área de profesión',
-      profession: 'Profesión',
+  skills: {
+    title: 'Habilidades',
+    skill: 'Habilidad',
+    less1year: 'Menos de 1 año',
+    '1to2years': '1 a 2 años',
+    '2to3years': '2 a 3 años',
+    '5to10years': '5 a 10 años',
+    '10plusyears': 'Más de 10 años',
+    job: 'Trabajo',
+    yearsOfExperience: 'Años de experiencia'
+  },
+  languages: {
+    title: 'Idiomas',
+    language: 'Idioma',
+    level: 'Nivel',
+    beginner: 'Principiante',
+    intermediate: 'Intermedio',
+    advanced: 'Avanzado',
+    native: 'Nativo'
+  }
+}
+
+export default defineI18nLocale(async () => {
+  return {
+    actions: {
+      add: 'Agregar',
+      edit: 'Editar',
+      delete: 'Eliminar',
+      save: 'Guardar',
+      cancel: 'Cancelar',
+      submit: 'Enviar',
+      options: 'Opciones'
+    },
+    common: {
+      home: 'Inicio',
+      about: 'Acerca de',
+      services: 'Servicios',
+      contact: 'Contacto',
+      testimonials: 'Testimonios',
+      blog: 'Blog',
+      portfolio: 'Portafolio',
+      privacyPolicy: 'Política de privacidad',
+      termsOfService: 'Términos de servicio',
       email: 'Correo electrónico',
-      phone: 'Teléfono',
-      address: 'Dirección',
-      summary: 'Resumen',
-      city: 'Ciudad',
-      country: 'País',
-      fillInformation: 'Rellena la información',
-      aboutMe: 'Acerca de mí'
+      noData: 'No hay datos disponibles'
     },
-    experience: {
-      title: 'Experiencia',
-      jobTitle: 'Título del trabajo',
-      company: 'Compañía',
-      position: 'Posición',
-      startDate: 'Fecha de inicio',
-      endDate: 'Fecha de finalización',
-      description: 'Descripción'
+    login: {
+      password: 'contraseña',
+      login: 'Iniciar sesión',
+      forgotPassword: '¿Olvidaste tu contraseña?',
+      rememberMe: 'Recuérdame',
+      signUp: 'Regístrate',
+      signIn: 'Iniciar sesión'
     },
-    education: {
-      title: 'Educación',
-      fieldOfStudy: 'Campo de estudio',
-      degree: 'Título',
-      institution: 'Institución',
-      startDate: 'Fecha de inicio',
-      endDate: 'Fecha de finalización',
-      secondary: 'Secundaria',
-      highSchool: 'Escuela secundaria',
-      technical: 'Técnica',
-      undergraduate: 'Licenciatura',
-      graduate: 'Posgrado',
-      masters: 'Maestría',
-      doctorate: 'Doctorado'
+    registration: {
+      username: 'Nombre de usuario',
+      password: 'Contraseña',
+      confirmPassword: 'Confirmar contraseña',
+      signUp: 'Regístrate'
     },
-    skills: {
-      title: 'Habilidades',
-      skill: 'Habilidad',
-      less1year: 'Menos de 1 año',
-      '1to2years': '1 a 2 años',
-      '2to3years': '2 a 3 años',
-      '5to10years': '5 a 10 años',
-      '10plusyears': 'Más de 10 años',
-      job: 'Trabajo',
-      yearsOfExperience: 'Años de experiencia'
-    },
-    languages: {
-      title: 'Idiomas',
-      language: 'Idioma',
-      level: 'Nivel',
-      beginner: 'Principiante',
-      intermediate: 'Intermedio',
-      advanced: 'Avanzado',
-      native: 'Nativo'
-    }
-  },
-  ...$vuetify
-} as TranslationKeys
+    profile: profile,
+    ...$vuetify
+  } as TranslationKeys
+})

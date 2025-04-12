4a42d51abe936f7eef3ab071538b1d559c8615cc
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-04-12 00:11:51 -0500

    feat(language): add language toggle menu for English and Spanish

---
+++ components/general/HeaderActionItems.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-menu>
+    <template v-slot:activator="{ props }">
+      <v-btn icon="mdi-translate" v-bind="props"></v-btn>
+    </template>
+    <v-list :selected="selected">
+      <v-list-item
+        v-for="locale in availableLocales"
+        @click="toggleLanguage(locale)"
+        :key="locale">
+        <v-list-item-title>{{ localItems[locale] }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { availableLocales, setLocale, locale } = useI18n()
+
+const selected = ref(locale.value)
+
+const localItems = {
+  en: 'English',
+  es: 'Spanish'
+}
+
+const toggleLanguage = (lang: any) => {
+  setLocale(lang)
+}
+</script>

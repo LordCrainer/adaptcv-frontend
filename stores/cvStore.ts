65d5a63e2815c97b85381a03236b8fa7ca51e30b
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-04-14 21:46:09 -0500

    feat(cvStore): refactor CV store to use state and actions for better data management

--- stores/cvStore.ts
+++ stores/cvStore.ts
@@ -1,12 +1,43 @@
 import { defineStore } from 'pinia'
-import type { ICV } from '~/features/profile'
+import type {
+  ICV,
+  IEducationItem,
+  ILanguageItem,
+  ISkill,
+  IUserProfile,
+  IWorkExperience
+} from '~/features/profile'
 
-export const useCvStore = defineStore('cv', () => {
-  const cvData = ref<ICV>()
-
-  function updateCVData(cv: ICV) {
-    cvData.value = cv
+export const useCVStore = defineStore('cv', {
+  state: () => ({
+    userProfile: {} as IUserProfile,
+    skills: [] as ISkill[],
+    languages: [] as ILanguageItem[],
+    education: [] as IEducationItem[],
+    workExperience: [] as IWorkExperience[],
+    status: 'draft' as 'draft' | 'published'
+  }),
+  actions: {
+    updateUserProfile(data: IUserProfile) {
+      this.userProfile = data
+    },
+    addSkill(skill: ISkill) {
+      this.skills.push(skill)
+    },
+    removeSkill(skill: string) {
+      this.skills = this.skills.filter((s) => s.skill !== skill)
+    },
+    addLanguage(language: ILanguageItem) {
+      this.languages.push(language)
+    },
+    addEducation(edu: IEducationItem) {
+      this.education.push(edu)
+    },
+    addWorkExperience(exp: IWorkExperience) {
+      this.workExperience.push(exp)
+    },
+    updateStatus(newStatus: 'draft' | 'published') {
+      this.status = newStatus
+    }
   }
-
-  return { cvData, updateCVData }
 })

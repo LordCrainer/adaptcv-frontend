7b93ba31a9434cd1f5b7481fad0dfbc1ee20c80a
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-04-20 11:23:44 -0500

    feat(Preview): remove unused data fetching logic and enhance profile display with skills and languages

--- pages/preview.vue
+++ pages/preview.vue
@@ -11,13 +11,6 @@ const location = computed(() => {
 function getLocation(...args: (string | undefined)[]) {
   return [...args].filter(Boolean).join(', ')
 }
-
-const data = ref<DEContentData>([])
-const $editor = ref<DragonEditor | null>(null)
-function getData() {
-  data.value = $editor.value?.getContentData() || []
-  console.log('data', data.value)
-}
 </script>
 
 <template>
@@ -25,13 +18,10 @@ function getData() {
     max-width="800"
     width="100%"
     class="mx-auto flex-card flex-container pa-4 text-caption">
-    <!-- <v-btn color="success" @click="getData">text</v-btn>
-    <ClientOnly>
-      <DragonEditor ref="$editor" />
-    </ClientOnly> -->
-    <v-card-title class="text-h5 font-weight-bold text-center">
+    <v-card-title class="text-h5 font-weight-bold text-center pb-0">
       {{ userProfile.name }}
     </v-card-title>
+
     <v-card-item class="text-center">
       {{ location }} -
       <a
@@ -47,12 +37,16 @@ function getData() {
     <v-card-item>
       {{ userProfile.summary }}
     </v-card-item>
+
     <v-card-item>
       <div class="text-body-2 font-weight-bold">
         {{ $t('profile.experience.title') }}
       </div>
       <v-divider></v-divider>
-      <v-col v-for="(exp, index) in workExperience" :key="index" class="pa-4">
+      <v-col
+        v-for="(exp, index) in workExperience"
+        :key="index"
+        class="py-2 px-0">
         <v-row>
           <v-col cols="8" md="8">
             <p class="font-weight-bold">
@@ -66,17 +60,19 @@ function getData() {
           </v-col>
         </v-row>
         <v-row>
-          <v-col>
+          <v-col class="pt-0">
             <DragonEditorViewer :content="exp.description" />
           </v-col>
         </v-row>
       </v-col>
     </v-card-item>
     <v-card-item>
-      <div class="text-body-2 font-weight-bold">Education</div>
+      <div class="text-body-2 font-weight-bold">
+        {{ $t('profile.education.title') }}
+      </div>
       <v-divider></v-divider>
       <v-row v-for="(edu, index) in education" :key="index" class="pa-4">
-        <v-col cols="8" md="8">
+        <v-col cols="8" md="8" class="py-2 px-0">
           <p class="font-weight-bold">
             {{ edu.institution }}
           </p>
@@ -85,6 +81,38 @@ function getData() {
         <v-col cols="4" md="4" class="text-right">
           <p>{{ getLocation(edu.city, edu.country) }}</p>
           <p>{{ edu.startDate }} - {{ edu.endDate }}</p>
+        </v-col>
+      </v-row>
+    </v-card-item>
+    <v-card-item>
+      <div class="text-body-2 font-weight-bold">
+        {{ $t('profile.skills.title') }}
+      </div>
+      <v-divider></v-divider>
+      <v-row class="pa-4">
+        <v-col cols="8" md="8">
+          <ul>
+            <li v-for="(skill, index) in skills" :key="index">
+              {{ skill.skill }} ({{
+                $t(`profile.skills.${skill.yearsOfExperience}`)
+              }})
+            </li>
+          </ul>
+        </v-col>
+      </v-row>
+    </v-card-item>
+    <v-card-item>
+      <div class="text-body-2 font-weight-bold">
+        {{ $t('profile.languages.title') }}
+      </div>
+      <v-divider></v-divider>
+      <v-row class="pa-4">
+        <v-col cols="8" md="8">
+          <ul>
+            <li v-for="(language, index) in languages" :key="index">
+              {{ language.name }} ({{ language.proficiency }})
+            </li>
+          </ul>
         </v-col>
       </v-row>
     </v-card-item>

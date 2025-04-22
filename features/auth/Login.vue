47c3aec83af464402cad97abc2ca33e36096bdce
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-04-22 01:11:01 -0500

    feat(Login): enhance login form with improved layout, validation, and password visibility toggle

--- features/auth/Login.vue
+++ features/auth/Login.vue
@@ -1,29 +1,77 @@
 <template>
-  <v-card class="pa-4">
-    <v-card-title class="text-h5 font-weight-bold">
-      {{ $t('login.title') }}
-    </v-card-title>
-    <v-card-text>
-      <v-form ref="form" v-model="valid" lazy-validation>
-        <v-text-field
-          v-model="form.email"
-          :rules="[rules.required(), rules.email()]"
-          label="Email"
-          required></v-text-field>
-        <v-text-field
-          v-model="form.password"
-          :rules="[rules.required(), rules.minLength()]"
-          label="Password"
-          type="password"
-          required></v-text-field>
-      </v-form>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn @click="login" :disabled="!valid" color="primary">
-        {{ $t('login.button') }}
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+  <v-form ref="form" lazy-validation>
+    <v-card class="pa-4 d-flex flex-column ga-4">
+      <v-card-title class="text-h5 font-weight-bold">
+        {{ $t('login.title') }}
+      </v-card-title>
+      <v-card-text class="d-flex flex-column">
+        <div class="d-flex flex-column ga-2">
+          <v-text-field
+            v-model="formData.email"
+            prepend-inner-icon="mdi-email"
+            :rules="[rules.email()]"
+            label="Email"
+            type="email"
+            variant="outlined"
+            aria-label="Email"
+            autocomplete="email"
+            aria-required="true"
+            required></v-text-field>
+          <v-text-field
+            v-model="formData.password"
+            prepend-inner-icon="mdi-lock"
+            :rules="[rules.required(), rules.minLength()]"
+            :type="showPassword ? 'text' : 'password'"
+            label="Password"
+            variant="outlined"
+            aria-label="Password"
+            aria-required="true"
+            required
+            autocomplete="current-password">
+            <template #append-inner>
+              <v-icon
+                slot="append"
+                @click="togglePasswordVisibility"
+                class="cursor-pointer">
+                {{ showPassword ? 'mdi-eye-off' : 'mdi-eye' }}
+              </v-icon>
+            </template>
+          </v-text-field>
+        </div>
+        <div class="d-flex justify-space-between align-center">
+          <v-checkbox
+            v-model="formData.remember"
+            label="Remember Me"
+            hide-details="auto"
+            color="primary"></v-checkbox>
+          <v-btn
+            variant="plain"
+            @click="forgotPassword"
+            class="text-secondary text-body-1 text-decoration-none">
+            {{ $t('actions.forgotPassword') }}
+          </v-btn>
+        </div>
+      </v-card-text>
+      <v-card-actions class="d-flex flex-column ga-4">
+        <v-btn
+          @click="login"
+          color="primary"
+          variant="flat"
+          style="width: 100%">
+          {{ $t('actions.signIn') }}
+        </v-btn>
+        <div>
+          {{ $t('login.dontHaveAccount') }}
+          <v-btn
+            variant="text"
+            @click="$router.push({ name: 'register' })"
+            class="text-secondary text-body-1 text-decoration-none">
+            {{ $t('actions.signUp') }}
+          </v-btn>
+        </div>
+      </v-card-actions>
+    </v-card>
+  </v-form>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
@@ -31,19 +79,31 @@ import { useRules } from '~/composables/useRules'
 
 const rules = useRules()
 
-const form = ref({
+const form = ref()
+
+const formData = ref({
   email: '',
-  password: ''
+  password: '',
+  remember: false
 })
 
-const valid = ref(false)
+const showPassword = ref(false)
+
+function forgotPassword() {
+  // Handle forgot password action
+  console.log('Forgot password clicked')
+}
 
 function login() {
-  if (valid.value) {
-    // Perform login action
-    console.log('Logging in with:', form.value)
-  } else {
-    console.error('Form is invalid')
-  }
+  console.log('Logging in with:', form.value)
+  // if (form.) {
+  //   // Perform login action
+  // } else {
+  //   console.error('Form is invalid')
+  // }
+}
+
+function togglePasswordVisibility() {
+  showPassword.value = !showPassword.value
 }
 </script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,18 @@
 <template>
-  <Login />
+  <div class="own-login">
+    <Login />
+  </div>
 </template>
 
 <script lang="ts" setup>
 import Login from '~/features/auth/Login.vue'
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.own-login {
+  width: 100%;
+  max-width: 600px;
+  margin: auto;
+}
+
+</style>

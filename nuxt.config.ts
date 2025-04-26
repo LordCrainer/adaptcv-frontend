9e9f9fca47dd9632bc40dea1c01c443c7b975ca8
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-04-25 23:26:56 -0500

    feat(styles): add base styles for profile photo and flex layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['@/assets/css/main.scss'],
+  css: ['@/assets/css/base.scss'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },debug: true,
   app: {

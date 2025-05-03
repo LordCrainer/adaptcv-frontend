278644d243fb81e3b14a8437e9fe42f1a752df3f
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-05-03 17:24:42 -0500

    fix: update footer link to open in a new tab and adjust class names

--- components/general/Footer.vue
+++ components/general/Footer.vue
@@ -1,12 +1,25 @@
 <template>
   <footer>
     <div
-      class="d-flex flex-column text-center white--text bg-grey darken-3 pa-2">
+      class="d-flex flex-column text-center bg-grey-darken-3 pa-2">
       <div>
         &copy; {{ new Date().getFullYear() }} - Powered by
-        <a href="https://github.com/LordCrainer">@LordCrainer</a>
+        <a href="https://github.com/LordCrainer" target="_blank">@LordCrainer</a>
       </div>
       <div>All rights reserved</div>
     </div>
   </footer>
 </template>
+
+<style scoped>
+footer a {
+  color: white;
+  text-decoration: underline;
+}
+footer a:hover {
+  color: yellow;
+}
+footer a:active {
+  color: white;
+}
+</style>

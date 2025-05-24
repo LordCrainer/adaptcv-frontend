2ce31e6adc395c0d8ae7e1b8f9d14b620b3b06f0
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-05-24 03:25:50 -0500

    refactor: remove unused builder installation and provider files

--- domains/builder/infraestructure/builder.provider.ts
+++
@@ -1,9 +0,0 @@
-import { createBuilderService } from '../services/create-builder.service'
-import { useApi } from '~/shared/http-client'
-
-export const builderProvider = () => {
-  const { api } = useApi()
-  return {
-    builderService: createBuilderService(api)
-  }
-}
--- plugins/di.ts
+++
@@ -1,5 +0,0 @@
-import { installBuilderProviders } from '~/domains/builder/infraestructure/builder.install'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  installBuilderProviders(nuxtApp.vueApp)
-})
